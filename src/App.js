21d86629f5295ccc94f5e91d843d1a42e7b2d5ee
yash <yashgouravkar@gmail.com>
2022-05-20 16:51:41 +0530

    dynamic question render(+axios)

--- src/App.js
+++ src/App.js
@@ -1,82 +1,37 @@
 import React, { useState, useEffect } from "react";
 import Quiz from "./Quiz";
 import Buttons from "./Buttons";
+import axios from "axios";
 
-function App() {
+const App = () => {
   const [game, setGame] = useState(false);
-  const [quizData, setQuizData] = useState([
-    { question: "", incorrect_answers: [], correct_answer: "" },
-  ]); //? save the API in empty array
-  let options = [];
-
-  //TODO :-- a way to display questions dynamically
-
-  console.log(quizData[0]);
-  //?define outside any function
-  const randomNum = Math.floor(Math.random() * quizData.length); //?generate a random number from the API array
-  options.push(quizData[randomNum].incorrect_answers); //!concat issue. options[0] is an array now
-  options.push(quizData[randomNum].correct_answer);
-
-  //? shuffle options
-  const shuffleArray = (arr) => {
-    for (let i = arr.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [arr[i], arr[j]] = [arr[j], arr[i]];
-    }
-  };
-  // shuffleArray(options);
-  // let [a, b, c] = options[0];
-  // let [e, f, g] = a;
-  options = [...options[0], options[1]]; //* now options is an array of 4
-  shuffleArray(options);
-
-  const randomQues = quizData[randomNum].question; //!there was some problem here...(in dot question)
-
-  console.log(options);
+  const [quizData, setQuizData] = useState([]); //? save the API in empty array
+  const allOptions = [];
   console.log(quizData);
 
   const startGame = () => {
     setGame(true);
   };
 
-  //?API Call using Fetch API. Have to use useEffect
+  const onSubmit = () => {};
   useEffect(() => {
-    fetch(
-      "https://opentdb.com/api.php?amount=10&category=11&difficulty=medium&type=multiple"
-    )
-      .then((res) => res.json())
-      .then((data) => setQuizData(data.results));
+    const fetchData = async () => {
+      const data = await axios.get(
+        "https://opentdb.com/api.php?amount=10&category=11&difficulty=medium&type=multiple"
+      );
+      setQuizData(data.data.results);
+    };
+    fetchData();
   }, []);
 
-  //!wrong way
-  // fetch(
-  //   "https://opentdb.com/api.php?amount=10&category=11&difficulty=medium&type=multiple"
-  // )
-  //   .then((res) => res.json())
-  //   .then((data) => setQuizData(data));//? every time the component renders it will re render the component, causing this infinite loop
-
-  // const questionArr = quizData.map((q) => (
-  //   <Quiz
-  //     key={Math.random().toString()}
-  //     data={JSON.stringify(quizData, null, 2)}
-  //     question={randomQues}
-  //     options={options}
-  //   />
-  // ));
-  // [a, b, c, d, e, f, g, h, i, j] = quizData;
-  // const { question, ...rest } = dataObj;
-  // qnArr = dataObj.forEach((obj) => obj.question);
-  const dataArray = () => {
-    let qnArr = [];
-    for (let i = 0; i < quizData.length; i++) {
-      qnArr = quizData[randomNum].question;
-      options.concat(quizData[randomNum].incorrect_answers);
-      options.push(quizData[randomNum].correct_answer);
-    }
+  const backgroundStyle = {
+    backgroundColor: game
+      ? "linear-gradient(45deg, #d05c9c, #e1664c, #66c3dd, #536eec)"
+      : "#c8ceda",
   };
 
   return (
-    <div className="App">
+    <div className="App" style={backgroundStyle}>
       {!game ? (
         <header className="App-header">
           <h1 className="main-title">Quizzical</h1>
@@ -87,18 +42,25 @@ function App() {
         </header>
       ) : (
         <div className="App-start">
-          <Quiz
-            // key={Math.random().toString()}
-            dataArray={dataArray()}
-            data={quizData}
-            question={randomQues}
-          />
-          <Buttons options={options} />
+          <ul>
+            {quizData.map((q) => {
+              return (
+                <li key={q.question}>
+                  <Quiz question={q.question} />
+                  <Buttons
+                    allOptions={[...q.incorrect_answers, q.correct_answer]}
+                  />
+                </li>
+              );
+            })}
+          </ul>
+          <button type="submit" className="btn--submit">
+            SUBMIT
+          </button>
         </div>
       )}
-      {/* <button type="submit">SUBMIT</button> */}
     </div>
   );
-}
+};
 
 export default App;
--- src/Buttons.js
+++ src/Buttons.js
@@ -1,49 +1,60 @@
 import React, { useState } from "react";
 
 const Button = (props) => {
-  const [click, setClick] = useState(false);
-  const clickHandler = () => {
-    setClick((prevClick) => !prevClick);
-    // {
-    //   click && { backgroundColor: "green" };
-    // }
+  const shuffleArray = (arr) => {
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
   };
+
+  shuffleArray(props.allOptions);
+  console.log(props.allOptions);
 
   return (
     <div className="quiz" id="quiz-btn">
+      {/* <ul>
+        {props.allOptions.map(option)=>{
+         return (
+                <li key={option} >
+                    <button type="button" className="btn--answer" >{option}</button>
+                </li>
+         );
+        }}
+      </ul> */}
       <button
         type="button"
-        onClick={clickHandler}
-        className="btn--answer"
+        // onClick={clickHandler}
+        className="btn--answer color"
         id="btn-1"
       >
-        {props.options[0]}
+        {props.allOptions[0]}
       </button>
       <button
         type="button"
-        onClick={clickHandler}
-        className="btn--answer"
+        // onClick={clickHandler}
+        className="btn--answer color"
         id="btn-2"
       >
-        {props.options[1]}
+        {props.allOptions[1]}
       </button>
       <button
         type="button"
-        onClick={clickHandler}
-        className="btn--answer"
+        // onClick={clickHandler}
+        className="btn--answer color"
         id="btn-3"
       >
-        {props.options[2]}
+        {props.allOptions[2]}
       </button>
       <button
         type="button"
-        onClick={clickHandler}
-        className="btn--answer"
+        // onClick={clickHandler}
+        className="btn--answer color"
         id="btn-4"
       >
-        {props.options[3]}
+        {props.allOptions[3]}
       </button>
-      <hr width="65%" />
+      <hr width="10%" />
     </div>
   );
 };
